2c9f5e58d239fea5529ac8181f5ecdb00406d871
pyosifovich <pyosifovich@vmware.com>
2015-11-09 16:47:53 +0200

    added missing IVdsServiceLoader

---
+++ Vds/Vds/Interop/IVdsServiceLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vds.Interop {
+	[ComImport, Guid("e0393303-90d4-4a97-ab71-e9b671ee2729"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	public interface IVdsServiceLoader {
+		[PreserveSig]
+		int LoadService([In, MarshalAs(UnmanagedType.LPWStr)] string machineName, out IVdsService vdsService);
+	}
+}
